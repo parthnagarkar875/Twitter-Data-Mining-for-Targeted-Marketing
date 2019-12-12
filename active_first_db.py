30180379fccf09e9cc1b6df252da4811c6c4547f
VirtualGoat <nagarkarparth875@gmail.com>
2019-12-12 12:50:39 +0530

    Storing data in database

---
+++ active_first_db.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 12:47:44 2019
+
+@author: Parth
+"""
+
+import active
+import collections
+from collections import Counter
+import pickle 
+import string
+import numpy as np
+import pandas as pd
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from math import ceil
+import tweepy
+
+
+
+url_list=list()
+username_list=list()
+user_profile_list=list()
+stored_tweets=list()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
