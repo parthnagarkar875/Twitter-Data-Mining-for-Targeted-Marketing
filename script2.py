b4b3d2f4a1c4784460c211ad0bf2775f01c8fb24
VirtualGoat <nagarkarparth875@gmail.com>
2019-12-20 17:26:11 +0530

    Extracting emails

--- script2.py
+++ script2.py
@@ -1,55 +1,46 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 18 17:21:46 2019
 
-@author: Parth
-"""
-import time
-import threading
-import urllib
 import re
-import io
-import active
-import re
-import sys
-from time import sleep
-import pickle
-import os
-from textblob import TextBlob
-from math import ceil
-from pathlib import Path
-import pandas as pd
-import concurrent.futures 
-import psycopg2
-import GetOldTweets3 as got
-import tweepy
+import smtplib
+import dns.resolver
 
-table= "keywords"
-count=0
-
-query="SELECT distinct id, name, username, tweet_text, created_at, location, polarity FROM public.keywords"
-
-try:     
-    conn = psycopg2.connect(database='Hiranandani', user = "postgres", password = "parth123n@#*", host = "127.0.0.1", port = "5432")    
-except:
-    print("Create database first")
-
-
-df=pd.read_sql(query,conn)
-
-
-
-mycursor = conn.cursor()
-
-
-for j,i in df.iterrows():
-    sql = "INSERT INTO {} (id, name, username, tweet_text, created_at, location, polarity) VALUES \
-        (%s, %s, %s, %s, %s, %s, %s)".format(table)
-    val = (i['id'], i['name'],i['username'],i['tweet_text'], i['created_at'], i['location'],i['polarity'])
-    mycursor.execute(sql, val)
-    conn.commit()
+def verify(inputAddress):
+    fromAddress= 'abc@gmail.com'
+    regex='^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
+    
+    addressToVerify= str(inputAddress)
+    
+    match=re.match(regex,addressToVerify)
+    
+    if match==None:
+        print('Bad syntax')
+        raise ValueError("Bad syntax")
+        
+    splitAddress= addressToVerify.split('@')
+    domain=str(splitAddress[1])
+    print("Domain:", domain)
+    
+    records= dns.resolver.query(domain,'MX')
+    mxRecord= records[0].exchange
+    mxRecord= str(mxRecord)
+    
+    server=smtplib.SMTP()
+    server.set_debuglevel(0)
+    
+    server.connect(mxRecord)
+    server.helo(server.local_hostname)
+    server.mail(fromAddress)
+    code,message =server.rcpt(str(addressToVerify))
+    server.quit()
+    
+    if code==250:
+        print("Success")
+    else:
+        print("Bad")
+    
 
 
-conn.close()
 
-
+for i in list1:
+    print(i)
+    verify(i)
+    
