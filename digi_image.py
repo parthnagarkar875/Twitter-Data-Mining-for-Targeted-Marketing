00902b6145de4fd716a49916c41c30fa58354939
VirtualGoat <nagarkarparth875@gmail.com>
2019-12-26 15:12:50 +0530

    Digital image rolling

--- digi_image.py
+++ digi_image.py
@@ -5,3 +5,8 @@ Created on Thu Dec 26 11:37:29 2019
 @author: Parth
 """
 
+import GetOldTweets3 as got
+
+tweetCriteria = got.manager.TweetCriteria().setUsername("@N_Hiranandani")\
+                                           .setMaxTweets(1000)
+tweet = got.manager.TweetManager.getTweets(tweetCriteria)
