97dd6b8aa60e0a03b6fe11ebb99224bfd7b4b1de
VirtualGoat <nagarkarparth875@gmail.com>
2019-12-27 12:11:58 +0530

    Digital image rolling

--- digi_image.py
+++ digi_image.py
@@ -4,7 +4,9 @@ Created on Thu Dec 26 11:37:29 2019
 
 @author: Parth
 """
+import nltk
 
+from nltk import word_tokenize
 import GetOldTweets3 as got
 from textblob import TextBlob
  
@@ -14,8 +16,26 @@ tweet = got.manager.TweetManager.getTweets(tweetCriteria)
 
 nounlist=list()
 for i in tweet:
-    blob = TextBlob(i.text)
-    for nouns in blob.noun_phrases:
-        nounlist.append(nouns)
-        
+    try:
+        text1 = active.deEmojify(i.text) 
+#         print(text1)
+        text=active.clean_tweet(text1)
+        clean.append(text)
+        blob = TextBlob(text)
+        for nouns in blob.noun_phrases:
+            nounlist.append(nouns)
+    except:
+        continue
 
+from nltk.tag import StanfordNERTagger
+
+st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
+
+
+
+
+import stanfordnlp
+stanfordnlp.download('en')   # This downloads the English models for the neural pipeline
+nlp = stanfordnlp.Pipeline() # This sets up a default neural pipeline in English
+doc = nlp("Barack Obama was born in Hawaii.  He was elected president in 2008.")
+doc.sentences[0].print_dependencies()
